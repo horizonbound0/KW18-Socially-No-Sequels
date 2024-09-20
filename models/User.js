6baab008034d2199d509a334f6869d36a96ddf06
Kenneth A Wagner <kenw2190@gmail.com>
2024-09-19 21:08:12 -0500

    added comments for model and route structures

--- models/User.js
+++ models/User.js
@@ -1,1 +1,10 @@
 
+// username - string, required, trimmed, unique
+
+// email - string, required, unique, must match a valid email address?
+
+// thoughts - array of _id values referencing the Thought model
+
+// friends - Array of _id values referencing the User model (self-reference)
+
+// virtual called friendCount that retrieves length of friernds on query
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,13 @@
 
+// GET all users
+
+// GET one user by _id and add friend and thought data
+
+// POST new user
+// POST to add a new friend to a user's friend list
+
+// PUT to update by _id
+
+// DELETE a user by _id
+// BONUS When a user is deleted, it removes their thoughts as well.
+// DELETE a friend from a user's friend list
